76eec4d354d3108f73ab160403c95eea62b5ede1
Valentin Berceaux <v.berceaux@gmail.com>
2022-12-08 09:37:48 +0100

    chore: basic routing with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello</h1>,
+  },
+  {
+    path: '/pokemon/:pokemonId',
+    element: <h1>Single pokemon</h1>,
+  },
+]);
+
 function App() {
-  return <h1 className="text-red-500">hello world</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
